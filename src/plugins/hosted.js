c3d028a4c6d32970a1cffae7f26d02a4e3db1817
dzigi <zygisbuzas@gmail.com>
2024-09-04 11:06:49 +0300

    asd

--- src/plugins/hosted.js
+++ src/plugins/hosted.js
@@ -1,6 +1,6 @@
 
 const LOCAL_URL = 'http://localhost:3001';
-const SERVER_URL = 'https://helsword.org:3001';
+const SERVER_URL = 'http://93.115.23.193:3001';
 const LOCAL_NET = 'http://192.168.0.141:3001'
 
 const baseUrl = SERVER_URL;
