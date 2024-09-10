7c518e1a9027f59ae96249e01e6416c3e60ce28c
dzigi <zygisbuzas@gmail.com>
2024-09-10 11:54:18 +0300

    asd

--- src/plugins/hosted.js
+++ src/plugins/hosted.js
@@ -3,7 +3,7 @@ const LOCAL_URL = 'http://localhost:3001';
 const SERVER_URL = 'https://93.115.23.193:3001';
 const LOCAL_NET = 'http://192.168.0.141:3001'
 
-const baseUrl = SERVER_URL;
+const baseUrl = LOCAL_URL;
 
 module.exports = {
     baseUrl
