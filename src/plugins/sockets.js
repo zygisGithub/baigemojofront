f8355a17120193b9c8bba0769970c29327bcbc37
dzigi <zygisbuzas@gmail.com>
2024-09-12 15:21:08 +0300

    asd

--- src/plugins/sockets.js
+++ src/plugins/sockets.js
@@ -1,9 +1,9 @@
 import { io } from 'socket.io-client';
 import config from './hosted';
 
-const socket = io('wss://www.helsword.org/socket.io/', {
-    transports: ['websocket']
+// Use the correct base URL for your WebSocket server
+const socket = io(config.baseUrl, {
+    transports: ['websocket'], // Ensure WebSocket transport is used
 });
-
 
 export default socket;
