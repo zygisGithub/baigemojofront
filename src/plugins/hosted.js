1493410a3333cce599c1516b6d92392500fbb3f5
zygisGithub <zygisbuzas@gmail.com>
2025-03-26 17:28:46 +0200

    fixed some issues

--- src/plugins/hosted.js
+++ src/plugins/hosted.js
@@ -2,8 +2,9 @@
 const LOCAL_URL = 'http://localhost:3001';
 const SERVER_URL = 'https://www.helsword.org:443';
 const LOCAL_NET = 'http://192.168.0.141:3001'
+const RENDER = 'https://baigemojoback.onrender.com'
 
-const baseUrl = LOCAL_URL;
+const baseUrl = RENDER;
 
 module.exports = {
     baseUrl
